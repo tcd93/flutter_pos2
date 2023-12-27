a449311df7412fcb83109dd9e4eef6fb14c406e3
tcd93 <dungtang93@gmail.com>
2023-12-27 09:06:57 +0700

    Config logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
+import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_pos/pages/main_section/page.dart';
 import 'package:flutter_pos/pages/report_section/report.dart';
 import 'package:flutter_pos/utils/app_theme.dart';
-import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logging/logging.dart';
 
 import 'pages/menu_section/menu.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  Logger.root.level = Level.ALL; // defaults to Level.INFO
+  Logger.root.onRecord.listen((record) {
+    print(
+      '${record.level.name} (${record.time}): ${record.message} ${record.error}',
+    );
+    if (record.stackTrace != null) print(record.stackTrace);
+    print('\n');
+  });
 
   runApp(
     ProviderScope(
