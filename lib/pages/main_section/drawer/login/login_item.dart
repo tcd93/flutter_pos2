8f92033e4457bc3f7d597340f86252b8dc5da7ce
tcd93 <dungtang93@gmail.com>
2024-02-19 11:21:10 +0700

    Add login instructions for web

--- lib/pages/main_section/drawer/login/login_item.dart
+++ lib/pages/main_section/drawer/login/login_item.dart
@@ -18,9 +18,83 @@ class LoginItem extends ConsumerWidget {
           await ref.read(loginProvider.notifier).logout();
           return;
         }
-        // for web, open navigate to url login page
+        // for web demo, show sample login and password so visitor can login
+        // then, open navigate to url login page
         if (kIsWeb) {
-          await ref.read(loginProvider.notifier).login();
+          final ok = await showAdaptiveDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog.adaptive(
+                // color primary
+                icon: const Icon(Icons.info_outline),
+                title: const Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Please configure project ',
+                      ),
+                      TextSpan(
+                        text: '.env',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' file to use your own login credentials, see ',
+                      ),
+                      TextSpan(
+                        text: '.env.example',
+                        // italic
+                        style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' file at project root.',
+                      )
+                    ],
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                content: SelectableRegion(
+                  focusNode: FocusNode(),
+                  selectionControls: MaterialTextSelectionControls(),
+                  child: const Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        children: [
+                          Text('Sample Username:  '),
+                          Expanded(child: Text('admin')),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text('Sample Password:  '),
+                          Expanded(child: Text('admin')),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context, false);
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context, true);
+                    },
+                    child: const Text('OK'),
+                  )
+                ],
+              );
+            },
+          );
+          if (ok) await ref.read(loginProvider.notifier).login();
           return;
         }
         Navigator.pushNamed(context, '/login');
